7afc8af7586c104d2e5be9300ccd566f8be7cecf
glacya <sunghyeck1779@gmail.com>
2025-10-04 20:20:39 +0900

    Implement database construction

--- bot/bot.py
+++ bot/bot.py
@@ -4,23 +4,58 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import asyncio
-import yt_dlp
+import json
+import random
+from pathlib import Path
 
 YDL_OPTIONS = {'format': 'bestaudio'}
 FFMPEG_OPTIONS = {'options': '-vn'}
 
+base_dir = Path(__file__).resolve().parent
+data_dir = base_dir.parent / "data"
+
+def check_admin():
+    pass
+
+def check_guild():
+    pass
+
+class Track:
+    def __init__(self, id: int, title: str, artist: str, yt_uri: str | None = None, yt_vid_title: str | None = None, yt_vid_length: int | None = None):
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.yt_uri = yt_uri
+        self.yt_vid_title = yt_vid_title
+        self.yt_vid_length = yt_vid_length
+
+    def __str__(self):
+        return f"{self.id}\t{self.title}\t{self.artist}\t{self.yt_uri}"
+
 class SongQuiz(commands.Cog):
-    def __init__(self, *_):
+    def __init__(self, bot):
         self.quiz_running = False
         self.quiz_match_songs_played = -1
         self.quiz_match_songs_total = -1
         self.quiz_sampled_songs = []
+        self.quiz_guild_id = None
+        self.quiz_text_channel_id = None
+        self.quiz_current_song_answer = None
+
+        self.bot: commands.Bot = bot
+        print(type(self.bot))
+
+        self.voice_client = None
+
+        self.song_database = []
+        self.load_song_database()
 
         super().__init__()
 
     @app_commands.command(name="노래퀴즈", description="노래 퀴즈를 시작합니다.")
     @app_commands.describe(song_count="퀴즈를 몇 곡 동안 진행할 것인지 적습니다. 최소 10, 최대 50.")
-    async def song_quiz_begin(self, interaction: discord.Interaction, song_count: int):
+    @app_commands.describe(use_random_offset="")
+    async def song_quiz_begin(self, interaction: discord.Interaction, song_count: int, use_random_offset: bool):
         if song_count < 10 or song_count > 50:
             await interaction.response.send_message("퀴즈는 최소 10곡, 최대 50곡까지만 할 수 있어요.")
 
@@ -32,6 +67,13 @@ class SongQuiz(commands.Cog):
             await interaction.response.send_message("이미 노래 퀴즈가 진행 중이에요. 퀴즈를 종료하고 싶다면 **/종료** 명령어로 퀴즈를 종료하세요.")
             return
 
+        self.quiz_text_channel_id = interaction.channel_id
+        self.quiz_guild_id = interaction.guild_id
+
+        print("Caller:")
+        print(self.quiz_text_channel_id)
+        print(interaction.guild_id)
+        print(interaction.user.id)
 
         await interaction.response.send_message("노래 퀴즈를 시작할게요!")
         await asyncio.sleep(1)
@@ -43,21 +85,18 @@ class SongQuiz(commands.Cog):
         self.quiz_running = True
 
         channel = interaction.user.voice.channel
-        voice_client = discord.utils.get(bot.voice_clients, guild=interaction.guild)
+        self.voice_client = discord.utils.get(self.bot.voice_clients, guild=interaction.guild)
 
         if voice_client is None:
             voice_client = await channel.connect()
 
-        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info("https://www.youtube.com/watch?v=OrrZ-TiTbPg", download=False)
-            audio_url = info['url']
-
         # TODO: Implement random song sampling
         # TODO: Revise code, so that it can stream from file
         # TODO: Revise code, so that it can give better UX
+        # TODO: Implement optional boolean flag, so that it can support random-offset song quiz
 
-        voice_client.play(discord.FFmpegPCMAudio(audio_url, **FFMPEG_OPTIONS))
-        await interaction.followup.send("노래 재생 중..\n**/제출** 명령어로 노래의 제목을 제출하세요.")
+        self.sample_song_pool(song_count)
+        self.run_quiz_problem()
     
     @app_commands.command(name="종료", description="노래 퀴즈를 종료합니다.")
     async def song_quiz_end(self, interaction: discord.Interaction):
@@ -77,17 +116,56 @@ class SongQuiz(commands.Cog):
 
     @app_commands.command(name="제출", description="노래 퀴즈의 답안을 제출합니다.")
     @app_commands.describe(answer="노래 제목의 정답. 띄어쓰기, 특수문자, 대소문자 등은 고려하지 않아도 됩니다.")
-    async def song_quiz_submit(self, interaction: discord.Integration, answer: str):
+    async def song_quiz_submit(self, interaction: discord.Interaction, answer: str):
         # TODO: Implement submit logic.
         # Use username to give points.
+        if not self.quiz_running:
+            await interaction.response.send_message("노래 퀴즈가 진행 중이지 않아서 제출할 수 없어요.")
+            return
+        
+        
 
         pass
 
+    @app_commands.command(name="스킵", description="현재 나오는 노래를 스킵하고 다음 문제로 넘어갑니다.")
+
     @app_commands.command(name="랭킹", description="노래 퀴즈의 랭킹을 봅니다.")
-    async def song_quiz_submit(self, interaction: discord.Integration):
+    async def song_quiz_submit(self, interaction: discord.Interaction):
         # TODO: Implement leaderboard logic.
 
         pass
+
+    @app_commands.command(name="새로고침", description="(관리자 전용) 곡 데이터베이스를 새로고침합니다.")
+    async def refresh_song_database(self, interaction: discord.Interaction):
+        # TODO: Implement refreshing.
+        pass
+
+    # Loads song data from songs.json.
+    def load_song_database(self):
+        songs_file = data_dir / "songs.json"
+
+        with open(songs_file, "r", encoding="utf-8") as f:
+            song_list = json.load(f)
+
+            for song in song_list:
+                track = Track(song["id"], song["title"], song["artist"], yt_uri=song["yt_uri"], yt_vid_title=song["yt_vid_title"], yt_vid_length=song["yt_vid_length"])
+
+                self.song_database.append(track)
+
+    # Samples song pool from song database.
+    async def sample_song_pool(self, song_count: int):
+        return random.sample(self.song_database, song_count)
+
+
+    # Run quiz problem.
+    async def run_quiz_problem(self):
+        guild = self.bot.get_guild(self.quiz_guild_id)
+        channel = self.quiz_text_channel_id(self.quiz_text_channel_id)
+        
+        # audio = discord.FFmpegPCMAudio(audio_file, **FFMPEG_OPTIONS)
+
+        # voice_client.play(audio)
+        # await interaction.followup.send(f"노래 재생 중! [문제 {self.quiz_match_songs_played + 1} / {self.quiz_match_songs_total}]\n**/제출** 명령어로 노래의 제목을 제출하세요.")
 
 class MiscPlayable(commands.Cog):
     @app_commands.command(name="쥐테스트", description="해당 인물의 쥐 여부를 판단합니다.")
--- data/data.py
+++ data/data.py
@@ -1,43 +1,44 @@
 import time
-import re
 import json
+import re
 import argparse
+import os
+import yt_dlp
 from pathlib import Path
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 base_dir = Path(__file__).resolve().parent
 song_path = base_dir / "songs.json"
+exclude_path = base_dir / "forbidden_artists.json"
+allowed_symbols = r"`~!@#$%^&*()_\-+=\[\]{}\\|;:'\",.<>/?"
+pattern = rf"[^\u0041-\u005A\u0061-\u007A\u00C0-\u024F\uAC00-\uD7A30-9{allowed_symbols}\s]"
+forbidden_regex = re.compile(pattern)
 
-YDL_OPTIONS = {'format': 'bestaudio', 'outtmpl': 'songs/%(id)s.opus'}
-
-
+YDL_OPTIONS = {'format': 'bestaudio', 'outtmpl': 'songs/%(id)s.opus', 'quiet': True}
 
 class Track:
-    def __init__(self, id: int, title: str, artist: str, yt_uri: str | None = None, yt_vid_title: str | None = None):
+    def __init__(self, id: int, title: str, artist: str, yt_uri: str | None = None, yt_vid_title: str | None = None, yt_vid_length: int | None = None, upvotes: int = 0, downvotes: int = 0):
         self.id = id
         self.title = title
         self.artist = artist
         self.yt_uri = yt_uri
         self.yt_vid_title = yt_vid_title
+        self.yt_vid_length = yt_vid_length
+        self.upvotes = upvotes
+        self.downvotes = downvotes
 
     def __str__(self):
         return f"{self.id}\t{self.title}\t{self.artist}\t{self.yt_uri}"
-    
-class Artist:
-    def __init__(self, name, gender):
-        self.name = name
-
-    def __str__(self):
-        return f"{self.name}"
 
 class SongManager:
     def __init__(self):
         self.tracks = []
         self.track_dist_set = set()
-        self.artists = {}
         self.track_new_id = 0
+        self.artists = set()
 
         options = Options()
         options.add_argument("--headless")
@@ -49,19 +50,30 @@ class SongManager:
         return f"{title}|{artist}"
 
     def load_crawled_entries(self):
-        with open(song_path, "r", encoding="utf-8") as f:
+        try:
+            f = open(song_path, "r", encoding="utf-8")
             json_data = json.load(f)
 
             for track_data in json_data:
                 yt_uri = None
+                yt_vid_length = None
+                yt_vid_title = None
                 if "yt_uri" in track_data:
                     yt_uri = track_data["yt_uri"]
+                    yt_vid_length = track_data["yt_vid_length"]
+                    yt_vid_title = track_data["yt_vid_title"]
 
-                track = Track(track_data["id"], track_data["title"], track_data["artist"], yt_uri=yt_uri)
+                track = Track(track_data["id"], track_data["title"], track_data["artist"], yt_uri=yt_uri, yt_vid_length=yt_vid_length, yt_vid_title=yt_vid_title)
                 self.tracks.append(track)
                 dist_str = SongManager.dist_str(track.title, track.artist)
                 self.track_dist_set.add(dist_str)
+                self.artists.add(track.artist)
+                self.track_new_id = max(self.track_new_id, track.id) + 1
 
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+        except:
+            pass
 
     def save_crawled_entries(self):
         elems = []
@@ -70,6 +82,32 @@ class SongManager:
 
         with open(song_path, "w", encoding="utf-8") as f:
             json.dump(elems, f, indent=2, ensure_ascii=False)
+
+    def show_artists(self):
+        for artist in self.artists:
+            print(artist, end=", ")
+
+    def filter_tracks(self):
+        print("Excluding pre-filtered artists, and songs with forbidden characters..")
+        with open(exclude_path, "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+
+            forbidden_artists = json_data["exclude"]
+
+            new_tracks = []
+
+            for track in self.tracks:
+                if track.artist in forbidden_artists:
+                    print(f"Track {track.title} is by forbidden artist {track.artist}")
+                    continue
+
+                if bool(forbidden_regex.search(track.title)):
+                    print(f"Track {track.title} - {track.artist} has forbidden character")
+                    continue
+
+                new_tracks.append(track)
+
+            self.tracks = new_tracks
 
     def query_melon_by_year(self, year: int):
         query_url_string = f"https://www.melon.com/chart/age/index.htm?chartType=YE&chartGenre=KPOP&chartDate={year}"
@@ -98,17 +136,56 @@ class SongManager:
             div_artist = row.find("div", class_="rank02")
             artist = div_artist.find("span", class_="checkEllipsis").text.rstrip()
 
-            dist_str = SongManager.dist_str()
+            dist_str = SongManager.dist_str(title, artist)
             
             if dist_str not in self.track_dist_set:
                 new_id = self.track_new_id
                 self.track_new_id += 1
                 self.track_dist_set.add(dist_str)
                 self.tracks.append(Track(new_id, title, artist))
+                self.artists.add(artist)
+
+                # print(f"{new_id}\t{title}\t{artist}")
+
 
     def query_melon_on_top100(self):
-        #TODO
-        pass
+        query_url_string = "https://www.melon.com/chart/index.htm"
+
+        self.driver.get(query_url_string)
+
+        res = self.driver.page_source
+
+        soup = BeautifulSoup(res, "html.parser")
+
+        rows50 = soup.find_all("tr", class_="lst50")
+        rows100 = soup.find_all("tr", class_="lst100")
+
+        for row in rows50 + rows100:
+            div_title = row.find("div", class_="rank01")
+
+            if div_title is None:
+                continue
+            
+            finder = div_title.find("a")
+
+            if finder is None:
+                continue
+
+            title = finder.text.rstrip()
+
+            div_artist = row.find("div", class_="rank02")
+            artist = div_artist.find("span", class_="checkEllipsis").text.rstrip()
+
+            dist_str = SongManager.dist_str(title, artist)
+            
+            if dist_str not in self.track_dist_set:
+                new_id = self.track_new_id
+                self.track_new_id += 1
+                self.track_dist_set.add(dist_str)
+                self.tracks.append(Track(new_id, title, artist))
+                self.artists.add(artist)
+
+                # print(f"{new_id}\t{title}\t{artist}")
 
     def query_melon(self):
         for year in range(2010, 2025):
@@ -119,6 +196,7 @@ class SongManager:
     def query_youtube_link(self, track: Track):
         search_keyword = f"{track.title} {track.artist} 가사"
         search_keyword = search_keyword.replace(" ", "+")
+        search_keyword = search_keyword.replace("#", "%23")
         query_url = f"https://www.youtube.com/results?search_query={search_keyword}"
 
         self.driver.get(query_url)
@@ -127,20 +205,87 @@ class SongManager:
         soup = BeautifulSoup(res, "html.parser")
 
         list_container = soup.find("ytd-two-column-search-results-renderer", class_="ytd-search")
-        vids = list_container.find_all("ytd-video-renderer", class_="ytd-item-section-renderer")
+        vid_blocks = list_container.find_all("ytd-video-renderer", class_="ytd-item-section-renderer")
 
-        with open("listdiv.html", "w", encoding="utf-8") as f:
-            f.write(vids[0].prettify())
+        # Use the first video.. but skip if the video is too long.
+        for vid_block in vid_blocks:
+            vid_dismissible = vid_block.find("div", id="dismissible", recursive=False)
+            vid_thumbnail = vid_dismissible.find("ytd-thumbnail")
+            vid_metadata = vid_dismissible.find("div", class_="text-wrapper")
 
-        # TODO: Get YT links. This work would be done sequentially.
-        # TODO: Also, get YT video title for future use.
+            vid_len_elem = vid_thumbnail.find("div", class_="yt-badge-shape__text").text
+            len_list = list(map(int, vid_len_elem.split(":")))
+
+            vid_uri_a = vid_block.find("a", class_="yt-simple-endpoint")
+            vid_href = vid_uri_a.attrs["href"]
+            vid_uri = vid_href.split("&")[0].split("=")[-1]
+
+            vid_title_a = vid_metadata.find("a", id="video-title")
+            vid_title = vid_title_a.attrs["title"]
+        
+            # print(vid_title)
+
+            vid_length = 0
+            weight = 1
+            for i in range(len(len_list)):
+                index = -1 - i
+                vid_length += len_list[index] * weight
+                weight *= 60
+            
+            # print(f"vid_length: {vid_length}")
+
+            if vid_length <= 600:
+                # Under 10 minutes: It should be correct video.
+
+                track.yt_vid_length = vid_length
+                track.yt_vid_title = vid_title
+                track.yt_uri = vid_uri
+
+                return
+
 
     def fetch_youtube_links(self):
-        self.query_youtube_link(self.tracks[-2])
+        batch = 0
+
+        for track in self.tracks:
+            if track.yt_uri is None:
+                self.query_youtube_link(track)
+                batch += 1
+
+                if batch % 10 == 0:
+                    print(f"Collected {batch}.. save.")
+                    self.save_crawled_entries()
+
+        self.save_crawled_entries()
+
+    def download_youtube_video_by_one(self, track: Track):
+        file_path = base_dir.parent / f"songs/{track.yt_uri}.opus"
+
+        if os.path.exists(file_path):
+            # If there is already a file, skip.
+            return
+
+        yt_url = f"https://www.youtube.com/watch?v={track.yt_uri}"
+        try:
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                ydl.download([yt_url])
+
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+        except:
+            print(f"Download failed on track {track.title}\n\t{yt_url}")
+
 
     def download_youtube_audios(self):
-        # TODO
-        pass
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            futures = {executor.submit(self.download_youtube_video_by_one, track): track for track in self.tracks}
+            success = 0
+
+            for _ in as_completed(futures):
+                success += 1
+
+                if success % 10 == 0:
+                    print(f"> Downloaded {success} / {len(self.tracks)}")
 
     def process_add_requests(self):
         # TODO
@@ -157,7 +302,9 @@ class SongManager:
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-m', "--melon", action="store_true", help="query melon chart to collect basic database of songs.")
-    arg_parser.add_argument('-y', "--youtube", action="store_true", help="find YT links, and downloads YT audio.")
+    arg_parser.add_argument('-y', "--youtube", action="store_true", help="find YT links for queried tracks.")
+    arg_parser.add_argument('-d', "--download", action="store_true", help="download audio files with given YT links.")
+    arg_parser.add_argument('-f', "--filter", action="store_true", help="filter artists with exclusion list 'forbidden_artists.json'")
     arg_parser.add_argument('-a', "--add", action="store_true", help="add new songs based on 'add_requests.json'")
     arg_parser.add_argument('-u', "--update", action="store_true", help="update existing songs based on 'update_requests.json'")
     arg_parser.add_argument('-r', "--remove", action="store_true", help="remove existing songs based on 'remove_requests.json'")
@@ -169,10 +316,20 @@ if __name__ == "__main__":
     if args.melon:
         manager.load_crawled_entries()
         manager.query_melon()
+        manager.filter_tracks()
+        manager.save_crawled_entries()
+
+    elif args.filter:
+        manager.load_crawled_entries()
+        manager.filter_tracks()
+        manager.save_crawled_entries()
 
     elif args.youtube:
         manager.load_crawled_entries()
         manager.fetch_youtube_links()
+
+    elif args.download:
+        manager.load_crawled_entries()
         manager.download_youtube_audios()
     
     elif args.add:
